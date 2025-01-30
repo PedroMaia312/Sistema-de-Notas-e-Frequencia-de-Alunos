42e91797e270f7d9fbafcc040d7f6f4113a702ed
unknown <pedromaiasilva17@gmail.com>
2025-01-29 21:13:20 -0300

    Projeto Spring Boot

---
+++ src/main/java/com/pedromaia/NotaA/NotaAApplication.java
@@ -0,0 +1,13 @@
+package com.pedromaia.NotaA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedromaia/NotaA/NotaAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromaia.NotaA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
